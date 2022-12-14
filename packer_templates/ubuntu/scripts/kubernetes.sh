6170395ecf946d65e918a18fc7e49810148038e1
Nikhil Nygaard <nik@nygaard.site>
2022-12-13 19:22:14 -0700

    kubenetes installation script

---
+++ packer_templates/ubuntu/scripts/kubernetes.sh
@@ -0,0 +1,25 @@
+#!/bin/sh -eux
+
+# https://linuxconfig.org/how-to-install-kubernetes-on-ubuntu-22-04-jammy-jellyfish-linux
+
+echo "Installing Kubernetes"
+
+apt update
+apt install docker.io
+systemctl enable docker
+systemctl start docker
+apt install apt-transport-https curl
+curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | apt-key add
+# apt-add-repository "deb http://apt.kubernetes.io/ kubernetes-xenial main"
+apt-add-repository "deb http://apt.kubernetes.io/ kubernetes-jammy main"
+apt install kubeadm kubelet kubectl kubernetes-cni
+swapoff -a
+# I'm think swap is turned off in the minification script, but I'm not sure yet.
+sed -i '/swapfile/d' /etc/fstab
+hostnamectl set-hostname kubernetes-master # This will have to be different for the worker node image
+kubeadm init
+mkdir -p $HOME/.kube
+cp -i /etc/kubernetes/admin.conf $HOME/.kube/config
+chown $(id -u):$(id -g) $HOME/.kube/config
+kubectl apply -f https://raw.githubusercontent.com/coreos/flannel/master/Documentation/kube-flannel.yml
+kubectl apply -f https://raw.githubusercontent.com/coreos/flannel/master/Documentation/k8s-manifests/kube-flannel-rbac.yml

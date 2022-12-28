8e79abbad09398cb9279bf0a758de04cc84bd0ad
Nikhil Nygaard <nik@nygaard.site>
2022-12-28 13:23:18 -0700

    turn off kubernetes installation for now

--- packer_templates/ubuntu/scripts/kubernetes.sh
+++ packer_templates/ubuntu/scripts/kubernetes.sh
@@ -4,21 +4,36 @@
 
 echo "Installing Kubernetes"
 
-apt install -y docker.io
-systemctl enable docker
-systemctl start docker
-apt install -y apt-transport-https curl
-curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | apt-key add
-apt-add-repository "deb http://apt.kubernetes.io/ kubernetes-xenial main"
-# apt-add-repository "deb http://apt.kubernetes.io/ kubernetes-jammy main"
-apt install -y kubeadm kubelet kubectl kubernetes-cni
-swapoff -a
-# I'm think swap is turned off in the minification script, but I'm not sure yet.
-sed -i '/swapfile/d' /etc/fstab
-hostnamectl set-hostname kubernetes-master # This will have to be different for the worker node image
-kubeadm init
-mkdir -p $HOME/.kube
-cp -i /etc/kubernetes/admin.conf $HOME/.kube/config
-chown $(id -u):$(id -g) $HOME/.kube/config
-kubectl apply -f https://raw.githubusercontent.com/coreos/flannel/master/Documentation/kube-flannel.yml
-kubectl apply -f https://raw.githubusercontent.com/coreos/flannel/master/Documentation/k8s-manifests/kube-flannel-rbac.yml
+# apt install -y docker.io
+# systemctl enable docker
+# systemctl start docker
+# apt install -y apt-transport-https curl
+# curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | apt-key add
+# apt-add-repository "deb http://apt.kubernetes.io/ kubernetes-xenial main"
+# # apt-add-repository "deb http://apt.kubernetes.io/ kubernetes-jammy main"
+# apt install -y kubeadm kubelet kubectl kubernetes-cni
+# swapoff -a
+# # I'm think swap is turned off in the minification script, but I'm not sure yet.
+# sed -i '/swapfile/d' /etc/fstab
+# hostnamectl set-hostname kubernetes-master # This will have to be different for the worker node image
+
+# cat > /etc/docker/daemon.json <<EOF
+# {
+#   "exec-opts": ["native.cgroupdriver=systemd"],
+#   "log-driver": "json-file",
+#   "log-opts": {
+#     "max-size": "100m"
+#   },
+#   "storage-driver": "overlay2"
+# }
+# EOF
+
+# systemctl daemon-reload
+# systemctl restart docker
+
+# kubeadm init
+# mkdir -p $HOME/.kube
+# cp -i /etc/kubernetes/admin.conf $HOME/.kube/config
+# chown $(id -u):$(id -g) $HOME/.kube/config
+# kubectl apply -f https://raw.githubusercontent.com/coreos/flannel/master/Documentation/kube-flannel.yml
+# kubectl apply -f https://raw.githubusercontent.com/coreos/flannel/master/Documentation/k8s-manifests/kube-flannel-rbac.yml

5c6a63935fd9a39936053e22f363aa07b0db84cb
Nikhil Nygaard <nik@nygaard.site>
2022-12-28 21:13:37 -0700

    turn off kubeadm init again

--- packer_templates/ubuntu/scripts/kubernetes.sh
+++ packer_templates/ubuntu/scripts/kubernetes.sh
@@ -10,9 +10,19 @@ apt install -y apt-transport-https curl wget
 
 # Install and Configure Docker
 apt install -y docker.io
-cat > /etc/docker/daemon.json <<EOF
+# cat > /etc/docker/daemon.json <<EOF
+# {
+#     "exec-opts": ["native.cgroupdriver=systemd"]
+# }
+# EOF
+cat <<EOF | sudo tee /etc/docker/daemon.json
 {
-    "exec-opts": ["native.cgroupdriver=systemd"]
+  "exec-opts": ["native.cgroupdriver=systemd"],
+  "log-driver": "json-file",
+  "log-opts": {
+    "max-size": "100m"
+  },
+  "storage-driver": "overlay2"
 }
 EOF
 cat /etc/docker/daemon.json
@@ -52,10 +62,10 @@ systemctl status containerd
 systemctl restart kubelet
 systemctl status kubelet
 
-kubeadm init --pod-network-cidr=10.244.0.0/16
+# kubeadm init --pod-network-cidr=10.244.0.0/16
 
-mkdir -p $HOME/.kube
-cp -i /etc/kubernetes/admin.conf $HOME/.kube/config
-chown $(id -u):$(id -g) $HOME/.kube/config
-kubectl apply -f https://raw.githubusercontent.com/coreos/flannel/master/Documentation/kube-flannel.yml
-kubectl apply -f https://raw.githubusercontent.com/coreos/flannel/master/Documentation/k8s-manifests/kube-flannel-rbac.yml
+# mkdir -p $HOME/.kube
+# cp -i /etc/kubernetes/admin.conf $HOME/.kube/config
+# chown $(id -u):$(id -g) $HOME/.kube/config
+# kubectl apply -f https://raw.githubusercontent.com/coreos/flannel/master/Documentation/kube-flannel.yml
+# kubectl apply -f https://raw.githubusercontent.com/coreos/flannel/master/Documentation/k8s-manifests/kube-flannel-rbac.yml

7065e4db9cec09f6c93391ad6e254dfee3b5d0c9
Nikhil Nygaard <nik@nygaard.site>
2023-01-16 13:40:02 -0700

    attempt to enable the serial console

--- packer_templates/ubuntu/scripts/networking.sh
+++ packer_templates/ubuntu/scripts/networking.sh
@@ -21,3 +21,6 @@ fi
 [ -e /etc/network/interfaces ] && sed -i 's/en[[:alnum:]]*/eth0/g' /etc/network/interfaces;
 sed -i 's/GRUB_CMDLINE_LINUX="\(.*\)"/GRUB_CMDLINE_LINUX="net.ifnames=0 biosdevname=0 \1"/g' /etc/default/grub;
 update-grub;
+
+# Enable Console from Virsh
+sudo systemctl enable --now serial-getty@ttyS0.service
